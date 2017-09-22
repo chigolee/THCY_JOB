06c04099e6438e02b80bb1b5d364c30881cd8b75
chigolee <13261726361@163.com>
2017-09-22 11:44:50 +0800

    更改能源设备数据接收

--- FK_08.05/Project/STM32F2xx_StdPeriph_Template/Top_User/B_Power_Operate.c
+++ FK_08.05/Project/STM32F2xx_StdPeriph_Template/Top_User/B_Power_Operate.c
@@ -45,57 +45,6 @@ CanRxMsg	Power_RecieveTrans;	//能源设备接收数据暂存
 /*----------能源设备数据接收------------*/
 void Task_CAN_Power_Recieve()
 {
-//收取数据
-if(Can1RxFBufPo!=Can1RxFBufPi)
-	{	
-	bcopy(&(Can1RxFBuf[Can1RxFBufPo]),&(Power_RecieveTrans),sizeof(Power_RecieveTrans));
-	Can1RxFBufPo=(Can1RxFBufPo+1)%Can1_TxF_BufLen;
-
-//----DC-DC 数据
-	if(Power_RecieveTrans.StdId<=0xFF)
-		{
-		switch(Power_RecieveTrans.StdId)
-			{
-		//DC-DC电压、电流、温度、状态
-			case 0x89:
-			case 0x8A:
-			case 0x8B:
-					{	DelaySecond_Healthy[1][4+Power_RecieveTrans.StdId-0x89]=0;	//超时计数置0 
-						bcopy(&(Power_RecieveTrans.Data[0]),&(g_YC_Package.NY.DCDCOutputVol[Power_RecieveTrans.StdId-0x89]),2);
-						bcopy(&(Power_RecieveTrans.Data[2]),&(g_YC_Package.NY.DCDCOutputCur[Power_RecieveTrans.StdId-0x89]),2);
-						g_YC_Package.NY.DCDC_Temprature[Power_RecieveTrans.StdId-0x89]=Power_RecieveTrans.Data[4];
-						g_YC_Package.NY.DCDC_State[Power_RecieveTrans.StdId-0x89]=Power_RecieveTrans.Data[6];
-						break;}
-
-		//太阳能阵列输入电压、电流
-			case 0x8C: 
-					{	DelaySecond_Healthy[1][4]=0;	//超时计数置0 
-						bcopy(&(Power_RecieveTrans.Data[0]),&(g_YC_Package.NY.Solar_Vol),2);
-						bcopy(&(Power_RecieveTrans.Data[2]),&(g_YC_Package.NY.Solar_Cur),2);
-						g_YC_Package.NY.HighDCWorkMode=Power_RecieveTrans.Data[4];
-						break;}
-		default:break;			
-			}
-		}
-
-//-----锂电池数据
-	else if(((Power_RecieveTrans.StdId&0x0F00)==0x300)||((Power_RecieveTrans.StdId&0x0F00)==0x200))
-		{
-		switch(Power_RecieveTrans.StdId&0x00F0)
-			{
-			case CANOperBigBet_1:	{DelaySecond_Healthy[2][0]=0;LionBetteryOperate(0);break;}
-			case CANOperBigBet_2:	{DelaySecond_Healthy[2][1]=0;LionBetteryOperate(1);break;}
-			
-			case 0x60:	
-			case 0x70:	
-			case 0x80:				{DelaySecond_Healthy[2][2]=0;LionBetteryOperate(2);break;}
-
-		default:break;
-			}
-		}
-	
-	}
-
 }
 
 
