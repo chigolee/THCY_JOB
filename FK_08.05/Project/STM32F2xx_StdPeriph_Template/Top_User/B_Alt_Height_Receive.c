43f85efb644cf29bf8bd9a944257bd0241306aa6
chigolee <13261726361@163.com>
2017-10-09 09:03:53 +0800

    填充Task_Alt_Height_Recieve函数内容

--- FK_08.05/Project/STM32F2xx_StdPeriph_Template/Top_User/B_Alt_Height_Receive.c
+++ FK_08.05/Project/STM32F2xx_StdPeriph_Template/Top_User/B_Alt_Height_Receive.c
@@ -45,6 +45,25 @@ void Modelname()
  /*---------气压高度表数据接收处理---------*/
 void Task_Alt_Height_Recieve()	
 {
+	{
+if(U9RxFBufPo!=U9RxFBufPi)
+	{
+	bcopy(&(U9RxFBuf[U9RxFBufPo][0]),&(Trans_Alt_Recieve),sizeof(Trans_Alt_Recieve));
+	U9RxFBufPo=(U9RxFBufPo+1)%U9_RxF_BufLen;
+	
+	if(Trans_Alt_Recieve.AddSum==AddSum1((DEF_UINT8*)&(Trans_Alt_Recieve.Hp_Byte1),10))
+		{
+		DelaySecond_Healthy[0][5]=0;		//超时计数置0 
+		
+		//处理数据
+		g_YC_Package.HD01.BaricAlt=(DEF_INT32)(((DEF_FLOAT32)((Trans_Alt_Recieve.Hp_Byte1)<<16)+(DEF_FLOAT32)((Trans_Alt_Recieve.Hp_Byte2)<<8)+(DEF_FLOAT32)(Trans_Alt_Recieve.Hp_Byte3))/10.0-300.0);
+		g_YC_Package.HD01.BaricAltPressure=(DEF_UINT16)((((DEF_FLOAT32)((Trans_Alt_Recieve.Hc_Byte1)<<16)+(DEF_FLOAT32)((Trans_Alt_Recieve.Hc_Byte2)<<8)+(DEF_FLOAT32)(Trans_Alt_Recieve.Hc_Byte3))/100.0-10.0)*100.0);
+		g_YC_Package.HD01.BaricAltUpDownSpeed=(DEF_INT32)(((DEF_FLOAT32)((Trans_Alt_Recieve.Hv_Byte1)<<8)+(DEF_FLOAT32)(Trans_Alt_Recieve.Hv_Byte2))/10.0-100.0);
+		g_YCLowPack.JC01.EnviTemp=(DEF_INT32)(((DEF_FLOAT32)((Trans_Alt_Recieve.Ts_Byte1)<<8)+(DEF_FLOAT32)(Trans_Alt_Recieve.Ts_Byte2))/10.0-70.0);
+
+		}
+	}
+}
 }
 
 
